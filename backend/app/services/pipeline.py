08f839ba7e794b84869275617cbfa3fcc0af42f0
Atharva45 <atharvaphanse403@gmail.com>
2026-05-01 12:14:33 +0530

    added youtube pipeline

--- backend/app/services/pipeline.py
+++ backend/app/services/pipeline.py
@@ -2,14 +2,18 @@ from app.scrapers.rss_scraper import fetch_news
 from app.services.save_articles import save_articles
 from app.services.summarize import summarize_articles
 from app.services.email_service import send_email
+from app.services.youtube_service import process_youtube
 
 def run_pipeline():
     print("Fetching news...")
     data = fetch_news()
     save_articles(data)
 
+    print("Processing YouTube...")
+    process_youtube()   
+
     print("Summarizing...")
-    summarize_articles()   
+    summarize_articles()
 
     print("Sending email...")
     send_email()
